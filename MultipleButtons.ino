fcf0d3fe4fa7e59cf9b8f1086e50a220bf6c6e1a
Blagojce Kolicoski <bill@thebrightbox.com>
2019-01-11 18:47:20 +0100

    Demonstration of how you can identify multiple buttons with a single analog input pin on Arduino

---
+++ MultipleButtons.ino
@@ -0,0 +1,21 @@
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int value = analogRead(A1);
+  if (value > 800 && value < 1000) {
+    Serial.println("Button1");
+  } else if (value > 750 && value < 800) {
+    Serial.println("Button2");
+  } else if (value > 650 && value < 750) {
+    Serial.println("Button3");
+  } else if (value > 480 && value < 650) {
+    Serial.println("Button4");
+  } else if (value < 480) {
+    Serial.println("Button5");
+  }
+  delay(500);
+}
